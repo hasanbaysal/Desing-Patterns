661ff6619d885cb7805a6e1211191206a1c69614
Hasan Baysal <33396565+hasanbaysal@users.noreply.github.com>
2022-12-11 21:17:34 +0300

    singleton dp kodları yazıldı

--- Singleton_Desing_Pattern/Program.cs
+++ Singleton_Desing_Pattern/Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace Singleton_Desing_Pattern
 {
+
+
+
+    /*
+     
+     Uygulama içerisnde bir nesnein bir kere yaratılmasını garanti altına almak
+    istediğimiz durumlarda bu patterni uyguluyoruz
+
+    1-) Uygulama boyunca tek bir örneğe sahip olmasını istediğimiz sınıfın
+      constructor'unı private hale getiriyoruz
+    2-) sınıf içerisidne sınıf o sınıf türünden referans tutan bir değişken tanımlıyoruz
+    3-) constructor artık dışarıdan erişme kapalı olduğu için contructor göreveri görecek bir static method yazıyoru
+
+     
+     */
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            //1.adım bir örnek oluşturuyoruz
+
+            // Single instance1 = new Single(); => Görüldüğü gibi constructor'a erişim yok
+
+            Single instance1 = Single.CreateObject();
+            instance1.Data = 10;
+
+            Console.WriteLine($"ilk oluşturalan nesnein datası {instance1.Data}");
+
+
+            Single instance2 = Single.CreateObject();
+
+            Console.WriteLine($"ikinci oluşturalan nesnein datası {instance2.Data}");
+            //biz herhangi bir data vermedik ama nesne zaten oluştuğu için yeniden bir 
+            //nesne yaratmayıp mecvut varol instance1'in bir referansını döndü
+         
+
         }
     }
 }
---
+++ Singleton_Desing_Pattern/Single.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Desing_Pattern
+{
+    public class Single
+    {
+        private static Single _single;
+
+        //Sınıf ait öylesine bir prop oluşuruyoruz
+        public int Data { get; set; }
+        private Single()
+        {
+            //constructor iptal edildi dışarıdan erişim yok artık
+        }
+
+        //constructor görevi görecek static method yazıyoruz
+
+
+        public static Single CreateObject()
+        {
+            if (_single == null)
+            {
+                _single = new Single();
+                return _single;
+            }
+            return _single;
+        }
+    }
+}
