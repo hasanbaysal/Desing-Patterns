57206cdb0115b5986579b742102a98b8428c33b7
Hasan Baysal <33396565+hasanbaysal@users.noreply.github.com>
2022-12-11 20:49:25 +0300

    proje adı değiştirildi

---
+++ Singleton_Desing_Pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Singleton_Desing_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
